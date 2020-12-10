c0d94681f4522abff0ab26acd7209e0b12aafec0
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2020-12-11 00:05:28 +0200

    Add Custom Query Relesed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,8 @@ class Movie < ApplicationRecord
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
+
+  def self.released
+    where('released_on < ?', Time.zone.now).order('released_on desc')
+  end
 end
