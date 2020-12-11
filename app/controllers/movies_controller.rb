490d145b8a0eefcca91f30f0925f4a96ca749d7c
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2020-12-11 08:13:08 +0200

    Add Admin Users

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class MoviesController < ApplicationController
+  before_action :require_signin, except: [:index, :show]
+  before_action :require_admin, except: [:index, :show]
+
   def index
     @movies = Movie.released
   end
@@ -16,7 +19,7 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params[:id])
     if @movie.update(movie_params)
-      redirect_to @movie, notice: 'Movie successfully updated!'
+      redirect_to @movie, notice: "Movie successfully updated!"
     else
       render :edit
     end
@@ -29,7 +32,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
     if @movie.save
-      redirect_to @movie, notice: 'Movie successfully created!'
+      redirect_to @movie, notice: "Movie successfully created!"
     else
       render :new
     end
@@ -38,13 +41,14 @@ class MoviesController < ApplicationController
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_url, alert: 'Movie successfully deleted!'
+    redirect_to movies_url, alert: "Movie successfully deleted!"
   end
 
   private
 
   def movie_params
-    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross,
-                                  :director, :duration, :image_file_name)
+    params.require(:movie).
+      permit(:title, :description, :rating, :released_on, :total_gross,
+             :director, :duration, :image_file_name)
   end
 end
